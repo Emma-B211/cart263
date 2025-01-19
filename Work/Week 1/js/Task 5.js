18e9946261ca83e18cc88c891b8afc59104fdac3
Emma-Beldick <emma.beldick@mail.concordia.ca>
2025-01-19 17:18:12 -0500

    finish task 5 finally

--- Work/Week 1/js/Task 5.js
+++ Work/Week 1/js/Task 5.js
@@ -8,23 +8,28 @@ const square = {
     fill: "#FFA500"
 
 }
-let radius = 30;
+// let radius = 30;
 let counter = 0;
-let circleX = 300;
-let circleY = 300;
-let circleFill = "white"
-const circle = {
-    x: 300,
-    y: 300,
-    size: 40,
-    radius: 30,
-    fill: (0, 0, 0, 100)
-}
-let ellipseAlpha = 1;
+// let circleX = 300;
+// let circleY = 300;
+// let circleFill = "white"
+// const circle = {
+//     x: 300,
+//     y: 300,
+//     size: 40,
+//     radius: 30,
+//     fill: (0, 0, 0, 100)
+// }
+//let ellipseAlpha = 0;
+const ellipseProps = {
+    radius: 50,
+    fill: "white",
+    alpha: 50,
 
+}
 function setup() {
     console.log("go")
-    createCanvas(600, 600);
+    createCanvas(400, 400);
 
 }
 
@@ -32,11 +37,31 @@ function draw() {
     background("black");
     displaySquare();
     //mousePressed();
-    checkCollisionWithSquare();
+    //  checkCollisionWithSquare();
     //mousePressed();
     // bigCircle();
-    drawCircle(circleX, circleY, 100, ellipseAlpha);
+    // drawCircle(circleX, circleY, 100, ellipseAlpha);
+    if (checkCollisionWithSquare()) {
+        square.fill = "#FFBF80";
+    } else {
+        square.fill = "#FFA500";
+    }
+    let x = 20;
+    let i = 0;
+    let alpha = ellipseProps.alpha
+    while (i < counter && counter <= 10 && counter >= 1) {
+        let currentRadius = ellipseProps.radius + (i * 30);
 
+
+        // draw ellipse
+        fill(255, 255, 255, alpha);
+        ellipse(200, 200, currentRadius, currentRadius);
+        alpha = alpha + 1
+        currentRadius = currentRadius + 50
+        // ellipseProps.radius = ellipseProps.radius + 0.5
+        x += 200;
+        i++;
+    }
 }
 
 function displaySquare() {
@@ -57,38 +82,41 @@ function displaySquare() {
 // function mouseHover() {
 
 // }
-function drawCircle(circleX, circleY, radius, ellipseAlpha) {
-    push();
-    fill(255, 255, 255, alpha);
-    ellipse(circleX, circleY, radius, ellipseAlpha);
-    pop();
-}
+// function drawCircle(circleX, circleY, radius, ellipseAlpha) {
+//     push();
+//     fill(255, 255, 255, alpha);
+//     ellipse(circleX, circleY, radius, ellipseAlpha);
+//     pop();
+// }
 
 function checkCollisionWithSquare() {
-    const mouseOverlap = (mouseX > square.x && mouseX < square.x + square.w && mouseY > square.y && mouseY < square.y + square.h);
-    if (mouseOverlap) {
-        square.fill = "#FFBF80";
-    } else {
-        square.fill = "#FFA500";
-    };
+    return mouseX > square.x && mouseX < square.x + square.w && mouseY > square.y && mouseY < square.y + square.h;
+
 
 }
 function mousePressed() {
     //console.log(counter);
-    const mouseOverlap = (mouseX > square.x && mouseX < square.x + square.w && mouseY > square.y && mouseY < square.y + square.h);
-    // console.log(counter);
-    // const mouseIsPressed = mousePressed();
-    if (mousePressed && mouseOverlap) {
-        counter = counter + 1;
-    };
-    // if (mouseOverlap) {
-    //     square.fill = "#FFBF80";
-    // } else {
-    //     square.fill = "#FFA500";
-    // };
-
+    if (checkCollisionWithSquare()) {
+        if (counter < 10) {
+            counter++;
+        }
+    }
 
 }
+// const mouseOverlap = (mouseX > square.x && mouseX < square.x + square.w && mouseY > square.y && mouseY < square.y + square.h);
+// // console.log(counter);
+// // const mouseIsPressed = mousePressed();
+// if (mousePressed && mouseOverlap) {
+//     counter = counter + 1;
+// };
+// if (mouseOverlap) {
+//     square.fill = "#FFBF80";
+// } else {
+//     square.fill = "#FFA500";
+// };
+
+
+
 
 // function bigCircle() {
 //     push();
