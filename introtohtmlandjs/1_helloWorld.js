18cc89aaca14af1a056011bf741d46501ccf12ea
Emma-Beldick <emma.beldick@mail.concordia.ca>
2025-01-21 14:21:10 -0500

    started dom

--- introtohtmlandjs/1_helloWorld.js
+++ introtohtmlandjs/1_helloWorld.js
@@ -1,1 +1,12 @@
-document.write("<h2>HELLO WORLD DYNAMIC</h2>");
+
+console.log("hello world")
+
+// window.onload=function(){
+
+// }
+window.onload=setup;
+
+function setup(){
+    console.log("hello")
+    //document.write('<p>HELLO WORLD DYNAMIC</p>');
+}
