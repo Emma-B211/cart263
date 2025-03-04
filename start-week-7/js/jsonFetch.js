bf852a1e6ad09bb9c0b05d175df9a3926a796aa7
Emma-Beldick <emma.beldick@mail.concordia.ca>
2025-03-04 13:58:50 -0500

    added new json file

--- start-week-7/js/jsonFetch.js
+++ start-week-7/js/jsonFetch.js
@@ -1,4 +1,15 @@
 window.onload = goApp;
 
 async function goApp(){
-console.log("hello fetch")}
+console.log("hello fetch")
+try{
+ 
+    let response = await fetch('../files/tests.json'); //response
+    let parsedResultJS = await response.json();
+    console.log(parsedResultJS)
+   }
+   catch(err){
+    
+       console.log(err)
+   }
+}
