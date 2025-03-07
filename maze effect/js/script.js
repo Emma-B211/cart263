2351644c6fcc0b3e28c32bef9486358f12f83a1a
Emma-Beldick <emma.beldick@mail.concordia.ca>
2025-03-07 00:17:16 -0500

    code works

--- maze effect/js/script.js
+++ maze effect/js/script.js
@@ -1,24 +1,65 @@
-/**
- * Title of Project
- * Author Name
- * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
- */
+let drips = [];
+{/* <script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.4.0/p5.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.4.0/addons/p5.sound.min.js"></script>
+</script> */}
 
-"use strict";
-
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
 function setup() {
-
+    createCanvas(1000, 650);
+    noStroke();
 }
 
-
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
 function draw() {
+    background(255); // White background to contrast ink
 
+    // Create new drips randomly
+    if (random(1) < 0.1) {
+        let x = random(width);
+        drips.push(new Drip(x, 0, random(5, 20)));
+    }
+
+    // Update and display all drips
+    for (let i = drips.length - 1; i >= 0; i--) {
+        drips[i].update();
+        drips[i].display();
+
+        // Remove drips that go beyond the screen
+        if (drips[i].y > height + 50) {
+            drips.splice(i, 1);
+        }
+    }
+}
+
+// Ink Drip Class
+class Drip {
+    constructor(x, y, size) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.speed = random(2, 5);
+        this.splatter = false; // Controls ink spreading
+    }
+
+    update() {
+        if (!this.splatter) {
+            this.y += this.speed;
+            if (this.y > height - 50) { // Adjust stopping height
+                this.splatter = true; // Start spreading ink
+            }
+        }
+    }
+
+    display() {
+        fill(0);
+        if (!this.splatter) {
+            ellipse(this.x, this.y, this.size, this.size * 2);
+        } else {
+            // Ink spread effect
+            for (let i = 0; i < 5; i++) {
+                let offsetX = random(-15, 15);
+                let offsetY = random(-10, 10);
+                ellipse(this.x + offsetX, this.y + offsetY, this.size * random(0.5, 1.5));
+            }
+        }
+    }
 }
